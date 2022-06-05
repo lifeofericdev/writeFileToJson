380cb96e561294018cada90e787292f0db8f4041
Eric Aidoo <ericaidoo@erics-mbp.myfiosgateway.com>
2022-06-05 17:52:41 -0400

    Creating new data and adding to JSON file

--- index.js
+++ index.js
@@ -2,17 +2,44 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+//middleware
 app.use(express.json());
-
+app.use(express.urlencoded({extended: false}))
 
 app.use(express.static('./public'));
 
 
-app.post('/api', (req, res) => {
-    res.send('post success');
-    // res.render('index', {incoming: ''})
-    console.log(req.body)
-    
+app.get('/', (req, res) => {
+    res.sendFile('index.hmtl', {root: __dirname})
+})
+
+//outgoing message/data
+var outgoingMessage = [];
+
+app.post('/create-data', (req, res) => {
+    const {company, employees} = req.body
+    const junk = { company: company, employees: employees };
+    res.send(company)
+    console.log(company);
+
+    //sending to JSON file
+fs.readFile("data.json", "utf8", function readFileCallback(err, data) {
+   if (err) {
+      console.log(err);
+   } else {
+      outboundMessage = JSON.parse(data); //now it an outboundMessageect
+
+      outboundMessage.push(junk); //add some data
+      json = JSON.stringify(outboundMessage, null, 2); //convert it back to json
+      fs.writeFile("data.json", json, "utf8", (error) => {
+         if (error) {
+            console.log("error " + error);
+         } else {
+            console.log("success");
+         }
+      }); 
+   }
+});
 })
 
 
